a69f0f5bc14fba242e0c20615b762d026f6990b3
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-01-21 14:48:11 -0300

    `test`: cria arquivo conftest para fixtures

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -1,13 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from src.app import app
-
-
-def test_read_root_retorna_ok_e_ola_mundo() -> None:
-    client = TestClient(app)
-
+def test_read_root_retorna_ok_e_ola_mundo(client) -> None:
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
