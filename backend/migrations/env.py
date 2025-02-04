4ad918dfd3566d422c9e9f4cc1e1100fe2b4cf08
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-03 22:49:09 -0300

    `feat`: add migration novas tabelas
    - Novo ponto de migration para a database.
    - Muda o comportamento do alembic para processamento em batch +
      comparação de tipos para tabelas já existentes.

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -52,6 +52,8 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
+        compare_type=True,
+        render_as_batch=True,
     )
 
     with context.begin_transaction():
@@ -73,7 +75,10 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            render_as_batch=True,
         )
 
         with context.begin_transaction():
