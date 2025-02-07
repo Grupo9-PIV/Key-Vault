0bebcd91cb0fd67043ac9786c53045a835d4ebca
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-06 21:22:30 -0300

    `refactor`: fixtures para objetos no db

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,13 +1,14 @@
 from collections.abc import Generator
+from datetime import datetime
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import Engine, create_engine
+from sqlalchemy import Engine, create_engine, event
 from sqlalchemy.orm import Session
 
 from src.app import app
 from src.database import table_registry
-from tests.helpers import add_license, add_user
+from src.models import License, Notification, RenewalRequest, User
 
 
 @pytest.fixture
@@ -18,6 +19,14 @@ def client() -> TestClient:
 @pytest.fixture
 def engine():
     engine = create_engine('sqlite:///:memory:')
+
+    # garante que a feature de foreign keys esteja ativada no sqlite
+    @event.listens_for(engine, 'connect')
+    def enable_foreign_keys(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute('PRAGMA foreign_keys=ON')
+        cursor.close()
+
     table_registry.metadata.create_all(engine)
 
     yield engine
@@ -32,13 +41,68 @@ def session(engine: Engine) -> Generator[Session, None, None]:
 
 
 @pytest.fixture
-def session_with_user(session: Session) -> Session:
-    add_user(session)
-    return session
+def user(session: Session) -> User:
+    user = User(
+        email='teste@teste.com',
+        password_hash='12345678',
+        name='Teste',
+        role='admin',
+        department='Teste',
+    )
+    session.add(user)
+    session.commit()
+    return user
 
 
 @pytest.fixture
-def session_with_user_and_license(session: Session) -> Session:
-    add_user(session)
-    add_license(session)
-    return session
+def mock_license(session: Session, user: User) -> License:
+    mock_license = License(
+        assigned_to_id=user.id,
+        manager_id=user.id,
+        software_name='XYZ Soft',
+        license_type='trial',
+        status='ativa',
+        developed_by='Test Soft',
+        version='v1.0.0',
+        purchase_date=datetime(1900, 1, 1),
+        start_date=datetime(1900, 1, 1),
+        end_date=datetime(1900, 1, 1),
+        license_key=None,
+        current_usage=None,
+        subscription_plan=None,
+        conditions=None,
+    )
+    session.add(mock_license)
+    session.commit()
+    return mock_license
+
+
+@pytest.fixture
+def notification(session: Session, user: User) -> Notification:
+    notification = Notification(
+        user_id=user.id,
+        license_id=None,
+        request_id=None,
+        message='Test Notification',
+    )
+    session.add(notification)
+    session.commit()
+    return notification
+
+
+@pytest.fixture
+def renewal_request(
+    session: Session, user: User, mock_license: License
+) -> RenewalRequest:
+    renewal_request = RenewalRequest(
+        license_id=mock_license.id,
+        requested_by_id=user.id,
+        manager_id=None,
+        reason=None,
+        feedback=None,
+        status='pendente',
+    )
+
+    session.add(renewal_request)
+    session.commit()
+    return renewal_request
