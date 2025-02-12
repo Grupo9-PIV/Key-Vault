fcc0cdb6fc487ea5ff5807c20e15657f06fb2834
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-12 14:53:47 -0300

    `feat`: add endpoint de token + testes
    Closes: #13

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,14 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
 
 from src.database import get_session
-from src.schemas import UserList, UserPublic, UserSchema
+from src.models import User
+from src.schemas import Token, UserList, UserPublic, UserSchema
+from src.security import create_access_token, verify_password
 from src.services import UserService
 
 app = FastAPI()
@@ -66,3 +70,21 @@ def delete_user(user_id: int, session: Session = Depends(get_session)):
         return deleted_user
     except ValueError as e:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=str(e))
+
+
+@app.post('/token', status_code=HTTPStatus.OK, response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session = Depends(get_session),
+):
+    user = session.scalar(select(User).where(User.email == form_data.username))
+
+    if not user or not verify_password(form_data.password, user.password_hash):
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST,
+            detail='Incorrect email or password',
+        )
+
+    access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': access_token, 'token_type': 'Bearer'}
