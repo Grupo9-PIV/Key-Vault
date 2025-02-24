c457f2b3d853fe80ba6f62e40a71341517498e24
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-24 20:23:24 +0000

    `feat`: refresh_access_token

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import select
 from src.models import User
 from src.schemas import Token
 from src.security import create_access_token, verify_password
-from src.types import T_OAuth2Form, T_Session
+from src.types import T_CurrentUser, T_OAuth2Form, T_Session
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -23,4 +23,11 @@ def login_for_access_token(session: T_Session, form_data: T_OAuth2Form):
 
     access_token = create_access_token(data={'sub': user.email})
 
-    return {'access_token': access_token, 'token_type': 'Bearer'}
+    return {'access_token': access_token}
+
+
+@router.post('/refresh_token', response_model=Token)
+def refresh_access_token(user: T_CurrentUser):
+    new_access_token = create_access_token(data={'sub': user.email})
+
+    return {'access_token': new_access_token}
