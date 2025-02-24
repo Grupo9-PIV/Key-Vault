75ba32fcbac2e09d22a92b72b4d7a1959632a901
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-24 19:39:51 +0000

    `test`: get token user not found

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -18,10 +18,22 @@ def test_get_token(client: TestClient, user: User) -> None:
     assert token['access_token']
 
 
-def test_get_token_bad_request(client: TestClient, user: User) -> None:
+def test_get_token_wrong_password(client: TestClient, user: User) -> None:
     response = client.post(
         '/auth/token',
         data={'username': user.email, 'password': 'wrong-password'},
+    )
+
+    result = response.json()
+
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert result['detail'] == 'Incorrect email or password'
+
+
+def test_get_token_user_not_found(client: TestClient, user: User) -> None:
+    response = client.post(
+        '/auth/token',
+        data={'username': 'not_real@user.com', 'password': user.password},
     )
 
     result = response.json()
