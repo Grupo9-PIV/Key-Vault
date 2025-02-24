eefc9285ea7bd1c21687f41a95d11c44568ed56b
Lynn <lynnpaes03@gmail.com>
2025-02-24 13:50:19 -0300

    feat: adiciona Bootstrap ao projeto e atualiza header e footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Footer = () => {
   return (
-    <footer style={footerStyle}>
-      <p>&copy; 2025 Key-Vault. Todos os direitos reservados.</p>
-      <p>
-        <a href="/privacy-policy" style={footerLinkStyle}>Política de Privacidade</a> | 
-        <a href="/terms" style={footerLinkStyle}>Termos de Uso</a>
-      </p>
-    </footer>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <img src="././public/logo.png" alt="logo" width="175" height="auto" class="d-block link-body-emphasis text-decoration-none dropdown-toggle"/>
+      </a>
+    </div>
+
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><p class="mb-3 mb-md-0 text-body-secondary">&copy; 2025 Key-Vault. Todos os direitos reservados</p></li>
+    </ul>
+  </footer>
+    //<footer style={footerStyle}>
+    //  <p>&copy; 2025 Key-Vault. Todos os direitos reservados.</p>
+    //  <p>
+    //    <a href="/privacy-policy" style={footerLinkStyle}>Política de Privacidade</a> | 
+    //    <a href="/terms" style={footerLinkStyle}>Termos de Uso</a>
+    //  </p>
+    //</footer>
   );
-};
-
-// Estilos para o Footer
-const footerStyle = {
-  backgroundColor: '#34495e',
-  color: '#fff',
-  padding: '10px 0',
-  textAlign: 'center',
-};
-
-const footerLinkStyle = {
-  color: '#fff',
-  textDecoration: 'none',
-  margin: '0 5px',
 };
 
 export default Footer;
