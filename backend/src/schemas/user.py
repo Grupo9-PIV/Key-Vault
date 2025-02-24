a587751393789f4ffaee79d1e0706bc8158ade13
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-24 18:59:03 -0300

    `feat`: add UserUpdate schema

--- backend/src/schemas/user.py
+++ backend/src/schemas/user.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict, EmailStr
 
 from src.enums import UserRole
@@ -18,6 +20,17 @@ class UserPublic(BaseModel):
     role: UserRole
 
     model_config = ConfigDict(from_attributes=True)
+
+
+class UserUpdate(BaseModel):
+    name: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+    role: Optional[UserRole] = None
+    department: Optional[str] = None
+
+    class Config:
+        extra = "forbid"  # evita campos extras não definidos
 
 
 class UserList(BaseModel):
