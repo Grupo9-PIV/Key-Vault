f0a33a641d5cb300663f62115eb6e7c7931a1424
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-02-25 18:03:32 -0300

    `style`: corrige a formatação de novos arquivos

--- backend/src/exceptions/license_exceptions.py
+++ backend/src/exceptions/license_exceptions.py
@@ -1,5 +1,4 @@
-#para dar a mensagem caso o usuário queira criar uma licença com um código que já existe
 class LicenseCodeAlreadyExistsException(Exception):
-    def __init__(self, message="Código da licença já existe"):
+    def __init__(self, message='Código da licença já existe'):
         self.message = message
         super().__init__(self.message)
