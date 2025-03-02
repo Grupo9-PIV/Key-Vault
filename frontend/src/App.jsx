b73d66c0e2fa01d76dfdd1088e8f16c5fef8445a
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-03-02 05:10:51 -0300

    `feat`: add integração entre front e backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import AppRoutes from './routes'; // Importação das rotas
+import api from './api';
+import AppRoutes from './routes';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get('/');
+        setMessage(response.data.message);
+        setError('');
+      } catch (err) {
+        setError('Erro ao carregar mensagem da API');
+        console.error('Detalhes do erro:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <AppRoutes /> {/* Renderiza o sistema de rotas */}
+      {/* Sistema de rotas */}
+      <AppRoutes />
+
+      <div
+        style={{ margin: '20px', padding: '15px', borderTop: '1px solid #ccc' }}
+      >
+        <h2>Status da API:</h2>
+        {loading ? (
+          <p>Verificando API...</p>
+        ) : error ? (
+          <p style={{ color: 'red' }}>{error}</p>
+        ) : (
+          <p>{message}</p>
+        )}
+      </div>
     </div>
   );
 }
