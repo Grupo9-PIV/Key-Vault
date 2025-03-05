0989fb5044ee0580274d6d001c70367b55cae22a
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-03-05 05:36:58 +0000

    `refactor`: types mais organizados

--- backend/src/types/__init__.py
+++ backend/src/types/__init__.py
@@ -1,18 +1,5 @@
-from typing import Annotated
-
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
-
-from src.database import get_session
-from src.models import User
-from src.security import get_current_user
-
+from .dependencies import T_CurrentUser, T_OAuth2Form, T_Session
 from .password import T_Password
-
-T_Session = Annotated[Session, Depends(get_session)]
-T_CurrentUser = Annotated[User, Depends(get_current_user)]
-T_OAuth2Form = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 __all__ = [
     'T_Session',
