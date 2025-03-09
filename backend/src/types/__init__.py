5377065ba50ee291607fbc555ebaea083fe3f4e2
João Vitor Alves <74667067+jvitor-alol@users.noreply.github.com>
2025-03-09 18:45:29 -0300

    `refactor`: mudança de nomenclatura PasswordType

--- backend/src/types/__init__.py
+++ backend/src/types/__init__.py
@@ -1,12 +1,12 @@
 from .dependencies import T_CurrentUser, T_OAuth2Form, T_Session
-from .password import T_Password
+from .password import PasswordType
 from .filters import T_RenewalRequestFilters, T_PaginationParams
 
 __all__ = [
+    'PasswordType',
     'T_Session',
     'T_CurrentUser',
     'T_OAuth2Form',
-    'T_Password',
     'T_RenewalRequestFilters',
     'T_PaginationParams',
 ]
