81a75078e530f19798a7b83468a3077c8412f95b
Maruiti <maruiti1@gmail.com>
2025-03-10 13:53:44 -0300

    adicionado partial update nos testes anteriores corrigido erro do utils

--- backend/tests/test_licenses.py
+++ backend/tests/test_licenses.py
@@ -1,149 +1,149 @@
 from datetime import datetime
+from http import HTTPStatus
 
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import select
 
-from src.enums import LicensePriority, LicenseStatus, LicenseType
 from src.models import License
+from tests.factory.faker import fake
+from tests.utils import get_license_data
 
-# Constantes para mensagens HTTP
-HTTP_OK = 200
-HTTP_NO_CONTENT = 204
+# from src.schemas.license import LicenseResponse
 
 
-def test_create_license(session: Session, create_license):
+def test_create_license(
+    session: Session, client: TestClient, token: str
+) -> None:
     """
     Testa a criação de uma licença usando a LicenseFactory.
     """
-    new_license = create_license(
-        software_name='Test Software',
-        license_type=LicenseType.TRIAL,
-        status=LicenseStatus.ATIVA,
-        developed_by='Test Corp',
-        version='v1.2.3',
-        priority=LicensePriority.ALTA,
-        purchase_date=datetime(2024, 1, 1),
-        start_date=datetime(2024, 1, 1),
-        end_date=datetime(2025, 1, 1),
+    license_data = get_license_data()
+
+    response = client.post(
+        '/licenses',
+        json=license_data,
+        headers={'Authorization': f'Bearer {token}'},
+    )
+    data = response.json()
+    
+    print("Resposta recebida:", response.json()) 
+
+    assert response.status_code == HTTPStatus.CREATED
+
+    db_license = session.scalar(
+        select(License).where(License.id == data['id'])
     )
 
-    assert new_license is not None
-    assert new_license.software_name == 'Test Software'
-    assert new_license.status == LicenseStatus.ATIVA
-    assert new_license.priority == LicensePriority.ALTA
+    assert (
+        db_license.software_name == license_data.get('software_name').lower()
+    )
+    assert db_license.purchase_date == datetime.fromisoformat(
+        license_data.get('purchase_date')
+    )
+    assert db_license.created_at is not None
+    assert db_license.updated_at is None
 
 
-def test_get_licenses(client: TestClient, token: str):
+def test_get_licenses_no_license(client: TestClient, token: str) -> None:
     """
     Testa a listagem de licenças via API.
     """
     response = client.get(
-        '/licenses/', headers={'Authorization': f'Bearer {token}'}
+        '/licenses', headers={'Authorization': f'Bearer {token}'}
     )
 
-    assert response.status_code == HTTP_OK
-    data = response.json()
-    assert isinstance(data, list)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == []
 
 
-def test_get_license_by_id(client: TestClient, token: str, create_license):
+def test_get_license_by_id(
+    client: TestClient, token: str, mock_license: License
+) -> None:
     """
     Testa a busca de uma licença específica pelo ID.
     """
-    new_license = create_license(software_name='Software Teste')
     response = client.get(
-        f'/licenses/{new_license.id}',
+        f'/licenses/{mock_license.id}',
         headers={'Authorization': f'Bearer {token}'},
     )
-
-    assert response.status_code == HTTP_OK
     data = response.json()
-    assert data['id'] == new_license.id
-    assert data['software_name'] == 'Software Teste'
+
+    assert response.status_code == HTTPStatus.OK
+    assert data['id'] == mock_license.id
+    assert data['version'] == mock_license.version
 
 
-def test_update_license(client: TestClient, token: str, create_license):
+def test_update_license(
+    client: TestClient, token: str, mock_license: License, session: Session
+) -> None:
     """
     Testa a atualização de uma licença.
     """
-    new_license = create_license(software_name='Software Antigo')
+    update_data = get_license_data()
+    update_data['license_key'] = fake.uuid4()
 
-    update_data = {
-        'software_name': 'windows',
-        'license_type': LicenseType.ASSINATURA.value,
-        'status': LicenseStatus.ATIVA.value,
-        'developed_by': 'Nova Empresa',
-        'start_date': '2025-01-01T00:00:00',
-        'end_date': '2025-12-31T23:59:59',
-        'created_at': '2025-01-01T00:00:00',
-        'license_key': '12345ABCDE67890FGHIJ11123',
-        'purchase_date': '2024-01-01',
-        'priority': LicensePriority.ALTA.value,
-    }
     response = client.put(
-        f'/licenses/{new_license.id}',
+        f'/licenses/{mock_license.id}',
         json=update_data,
         headers={'Authorization': f'Bearer {token}'},
     )
+    data = response.json()
 
-    print(response.status_code)
-    print(response.json())  # Veja o erro detalhado
+    db_license = session.scalar(
+        select(License).where(License.id == data.get('id'))
+    )
 
-    assert response.status_code == HTTP_OK
-    updated_license = response.json()
-    assert updated_license['software_name'] == 'windows'
-    assert updated_license['license_key'] == '12345ABCDE67890FGHIJ11123'
+    assert response.status_code == HTTPStatus.OK
+    assert data['purchase_date'] == update_data['purchase_date']
+
+    assert update_data['software_name'] == db_license.software_name
+    assert update_data['license_key'] == db_license.license_key
+    assert db_license.updated_at is not None
 
 
 def test_partial_update_license(
-    client: TestClient, token: str, create_license
-):
+    client: TestClient, token: str, mock_license: License, session: Session
+) -> None:
     """
-    Testa a atualização parcial de uma licença (PATCH).
+    Testa a atualização parcial de uma licença.
     """
-    new_license = create_license(
-        software_name='windows',
-        status=LicenseStatus.EXPIRADA,
-    )
-
-    partial_update_data = {'status': 'ativa', 'developed_by': 'Nova Empresa'}
-
+    partial_update_data = {"software_name": "NovoSoftware"}
+    
     response = client.patch(
-        f'/licenses/{new_license.id}',
+        f'/licenses/{mock_license.id}',
         json=partial_update_data,
         headers={'Authorization': f'Bearer {token}'},
     )
-
-    assert response.status_code == HTTP_OK, response.json()
-
-    updated_license = response.json()
-    assert updated_license['status'] == 'ativa'
-    assert updated_license['developed_by'] == 'Nova Empresa'
-    assert updated_license['software_name'] == 'windows'
+    data = response.json()
+    
+    db_license = session.scalar(
+        select(License).where(License.id == data.get('id'))
+    )
+    
+    assert response.status_code == HTTPStatus.OK
+    assert data['software_name'] == partial_update_data['software_name']
+    assert db_license.software_name == partial_update_data['software_name']
+    assert db_license.updated_at is not None
 
 
 def test_delete_license(
-    client: TestClient, token: str, create_license, session: Session
+    client: TestClient, token: str, mock_license: License, session: Session
 ):
     """
     Testa a exclusão de uma licença.
     """
-    # Cria uma nova licença
-    new_license = create_license()
+    # license_schema=LicenseResponse.model_validate(mock_license).model_dump()
 
-    # Realiza a requisição DELETE
     response = client.delete(
-        f'/licenses/{new_license.id}',
+        f'/licenses/{mock_license.id}',
         headers={'Authorization': f'Bearer {token}'},
     )
 
-    # Verifica se o status da resposta é 204 (sem conteúdo)
-    assert (
-        response.status_code == HTTP_NO_CONTENT
-    )  # Código 204 indica sucesso sem conteúdo
+    deleted_license = session.scalar(
+        select(License).where(License.id == mock_license.id)
+    )
 
-    # Verifica se a licença foi removida do banco de dados
-    deleted_license = session.get(License, new_license.id)
-    assert (
-        deleted_license is None
-    )  # A licença deve ser removida do banco de dados
+    assert response.status_code == HTTPStatus.NO_CONTENT
+    # assert response.json() == license_schema
+    assert deleted_license is None
